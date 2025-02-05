65815107cf28c820b2e1e08cd2a58a462aa0635b
Mahdi Torabi <123413512+Mtorabik@users.noreply.github.com>
2025-02-05 18:11:10 +0330

    2nd

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function typeEffect(element, text, speed) {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    typeEffect(document.getElementById("typedText"), "Welcome to My Portfolio...", 100);
+    typeEffect(document.getElementById("typedText"), "Welcome to My Portfolio", 100);
     typeEffect(document.getElementById("aboutMe"), "My name is Mahdi Torabi and I am studying electrical engineering at Shahid Beheshti Unversity in Tehran. I am a developer of Apple products and a game developer with unreal engine.", 50);
     typeEffect(document.getElementById("interests"), "I enjoy working with AI and playing along with game analysis and discovering the world's latest technologies 🎮🤖", 50);
     typeEffect(document.getElementById("goals"), "Building unique apps & games to make an impact. 🚀", 50);
